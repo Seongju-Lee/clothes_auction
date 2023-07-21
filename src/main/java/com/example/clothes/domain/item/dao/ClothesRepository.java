69ab584a214ddd073aea622da1d91fe2c3ae797a
seongju <lsj40413@gmail.com>
2023-07-21 16:59:32 +0900

    feat: clothes dao

---
+++ src/main/java/com/example/clothes/domain/item/dao/ClothesRepository.java
@@ -0,0 +1,19 @@
+package com.example.clothes.domain.item.dao;
+
+import com.example.clothes.domain.item.domain.Clothes;
+import com.example.clothes.domain.item.domain.ClothesCategory;
+import com.example.clothes.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ClothesRepository extends JpaRepository<Clothes, Long> {
+
+    List<Clothes> findByCategory(ClothesCategory category);
+    List<Clothes> findBySellerId(Long sellerId);
+
+    @Query("SELECT c FROM Clothes c WHERE c.category in :categories")
+    List<Clothes> findByCategories(@Param("categories") List<ClothesCategory> categories);
+}
