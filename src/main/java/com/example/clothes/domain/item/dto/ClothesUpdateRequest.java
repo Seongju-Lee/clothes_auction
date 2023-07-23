e0e146483a6a07a154fa1bd04e03045273afc8ac
seongju <lsj40413@gmail.com>
2023-07-23 17:24:05 +0900

    refactor: Clothes update로직 변경
    - @Builder 애노테이션 적용
    - ClothesCategory -> String으로 변경

--- src/main/java/com/example/clothes/domain/item/dto/ClothesUpdateRequest.java
+++ src/main/java/com/example/clothes/domain/item/dto/ClothesUpdateRequest.java
@@ -3,6 +3,7 @@ package com.example.clothes.domain.item.dto;
 import com.example.clothes.domain.item.domain.ClothesCategory;
 
 public record ClothesUpdateRequest(
+        Long clothesId,
         String name,
         String description,
         String imgSrc,
--- src/main/java/com/example/clothes/domain/item/service/ClothesService.java
+++ src/main/java/com/example/clothes/domain/item/service/ClothesService.java
@@ -2,18 +2,19 @@ package com.example.clothes.domain.item.service;
 
 import com.example.clothes.domain.item.dao.ClothesRepository;
 import com.example.clothes.domain.item.domain.Clothes;
-import com.example.clothes.domain.item.domain.ClothesBuilder;
 import com.example.clothes.domain.item.domain.ClothesCategory;
 import com.example.clothes.domain.item.dto.ClothesResponse;
 import com.example.clothes.domain.item.dto.ClothesRequest;
 import com.example.clothes.domain.item.dto.ClothesUpdateRequest;
 import com.example.clothes.domain.user.dao.UserRepository;
 import com.example.clothes.domain.user.domain.User;
+import com.example.clothes.domain.user.domain.UserType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
@@ -24,9 +25,19 @@ public class ClothesService {
     private final UserRepository userRepository;
 
     public ClothesResponse save(ClothesRequest request) {
-        User seller = userRepository.findById(request.sellerId()).get();
+        User seller = userRepository.findById(request.sellerId())
+                .filter(user -> UserType.isSeller(user.getType()))
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않거나 상품등록 권한이 없습니다."));
+
+        ClothesCategory category = ClothesCategory.fromString(request.category().toUpperCase());
         Clothes savedClothes = clothesRepository.save(
-                new Clothes(seller, request.name(), request.description(), request.imgSrc(), request.category())
+                Clothes.builder()
+                        .seller(seller)
+                        .name(request.name())
+                        .description(request.description())
+                        .imgSrc(request.imgSrc())
+                        .category(category)
+                        .build()
         );
         return ClothesResponse.fromEntity(savedClothes);
     }
@@ -35,30 +46,56 @@ public class ClothesService {
         clothesRepository.deleteById(clothesId);
     }
 
-    public ClothesResponse update(ClothesUpdateRequest request, Long clothesId) {
+    public ClothesResponse update(ClothesUpdateRequest request) {
 
-        Clothes clothes = clothesRepository.findById(clothesId).get();
-        Clothes updatedClothes = new ClothesBuilder()
-                .name(request.name())
-                .description(request.description())
-                .imgSrc(request.imgSrc())
-                .category(request.category())
-                .buildToUpdate(clothes);
+        Clothes clothes = clothesRepository.findById(request.clothesId())
+                .orElseThrow(() -> new NoSuchElementException("상품이 존재하지 않습니다."));
+
+        Clothes updatedClothes = Clothes.builder()
+                .itemId(clothes.getItemId())
+                .seller(clothes.getSeller())
+                .name(request.name() != null ? request.name() : clothes.getName())
+                .description(request.description() != null ? request.description() : clothes.getDescription())
+                .imgSrc(request.imgSrc() != null ? request.imgSrc() : clothes.getImgSrc())
+                .category(request.category() != null ? request.category() : clothes.getCategory())
+                .build();
         return ClothesResponse.fromEntity(clothesRepository.save(updatedClothes));
     }
 
-    public List<ClothesResponse> findByCategory(ClothesCategory category) {
-        List<Clothes> manyClothes = clothesRepository.findByCategory(category);
-        return ClothesResponse.fromEntities(manyClothes);
+    public List<ClothesResponse> findByCriteria(
+            List<String> categories,
+            Long sellerId
+    ) {
+        if (categories == null && sellerId == null) {
+            return findAll();
+        } else if (categories != null && sellerId == null) {
+            return findByCategories(categories);
+        } else if (categories == null) {
+            return findBySellerId(sellerId);
+        } else {
+            throw new IllegalArgumentException("올바르지 못한 요청입니다.");
+        }
     }
 
-    public List<ClothesResponse> findByCategories(List<ClothesCategory> categories) {
+
+    private List<ClothesResponse> findByCategories(List<String> categoryParam) {
+
+        List<ClothesCategory> categories = categoryParam.stream()
+                .map(ClothesCategory::fromString)
+                .toList();
         List<Clothes> manyClothes = clothesRepository.findByCategories(categories);
         return ClothesResponse.fromEntities(manyClothes);
     }
 
-    public List<ClothesResponse> findBySellerId(Long sellerId) {
-        List<Clothes> manyClothes = clothesRepository.findBySellerId(sellerId);
+    private List<ClothesResponse> findBySellerId(Long sellerId) {
+        User seller = userRepository.findById(sellerId)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 판매자입니다."));
+        List<Clothes> manyClothes = clothesRepository.findBySeller(seller);
         return ClothesResponse.fromEntities(manyClothes);
+    }
+
+    private List<ClothesResponse> findAll() {
+        List<Clothes> clothes = clothesRepository.findAll();
+        return ClothesResponse.fromEntities(clothes);
     }
 }
